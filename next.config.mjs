6f61c4493b9b04d9c3d569e5667a14a2e35fb600
YosefGunawan <gunawanyosef21@gmail.com>
2025-07-11 15:49:36 +0700

    Add Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/Experience_AI_LandingPage" : "",
+  assetPrefix: isProd ? "/Experience_AI_LandingPage/" : "",
+};
 
 export default nextConfig;
